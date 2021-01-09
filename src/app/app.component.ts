8f72647f95163a67966b4bdd85d71f3cd76829aa
mjs001 <maljayd42@gmail.com>
2021-01-09 08:25:07 -0600

    added dragndrop ability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 import { Task } from './task/task';
-
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop'
+import {MatDialog, MAT_DIALOG_SCROLL_STRATEGY_FACTORY} from '@angular/material/dialog'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,4 +13,30 @@ export class AppComponent {
     {title: 'Learn C', description: 'Learn C by doing project tutorials'},
     {title: 'Create Kanban Board', description: 'Develop Kanban Board in Angular'}
   ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+constructor(private dialog: MatDialog) {}
+newTask(): void {
+  const dialogRef = this.dialog.open(TaskDialogComponent, {
+    width: '270px',
+    data: {
+      task: {},
+    },
+  });
+  dialogRef .afterClosed() .subscribe((result: TaskDialogResult) => this.todo.push(result.task))
+}
+
+  editTask(list: string, task: Task): void {}
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  } else {
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DragDropModule} from '@angular/cdk/drag-drop'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { TaskComponent } from './task/task.component';
 import {MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button'
+import {MatDialogModule} from '@angular/material/dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
