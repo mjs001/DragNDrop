db4de111980972aabd91387bd6530d790db7d013
mjs001 <maljayd42@gmail.com>
2021-01-09 12:41:41 -0600

    added in edit and delete functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,61 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
-import {MatDialog} from '@angular/material/dialog';
+import {MatDialog, MAT_DIALOG_SCROLL_STRATEGY_FACTORY} from '@angular/material/dialog';
 import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 import { TaskDialogResult} from './task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   todo: Task[] = [
-    {title: 'Learn C', description: 'Learn C by doing project tutorials'},
-    {title: 'Create Kanban Board', description: 'Develop Kanban Board in Angular'}
+    {
+      title: 'Buy milk',
+      description: 'Go to the store and buy milk',
+    },
+    {
+      title: 'Create a Kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!',
+    },
   ];
   inProgress: Task[] = [];
   done: Task[] = [];
-constructor(private dialog: MatDialog) {}
-newTask(): void {
-  const dialogRef = this.dialog.open(TaskDialogComponent, {
-    width: '270px',
-    data: {
-      task: {},
-    },
-  });
-  dialogRef .afterClosed() .subscribe((result: TaskDialogResult) => this.todo.push(result.task))
-}
 
-  editTask(list: string, task: Task): void {}
+  constructor(private dialog: MatDialog) {}
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
+  }
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
   drop(event: CdkDragDrop<Task[]>): void {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
